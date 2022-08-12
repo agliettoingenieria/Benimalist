6108c72320be4862bceb483d69c24ff31615936c
IgnFed <ignaciofedorenco2317@gmail.com>
2022-08-12 12:37:01 -0300

    Arreglado error que no muestra boton de whatsapp

--- scripts/home.js
+++ scripts/home.js
@@ -1,4 +1,4 @@
-const main = $("main");
+const mainImgContainer = $(".img-container");
 const burgerBtn = $("#menu_btn");
 const linksList = $(".links_list__list");
 const themeBtn = $("#theme_btn");
@@ -7,7 +7,7 @@ const image = new Image();
 let load = 0;
 const interval = setInterval(blurry, 20);
 image.addEventListener("load", (e) => {
-	main.appendChild(image);
+	mainImgContainer.insertAdjacentElement("afterbegin", image);
 	image.setAttribute("decoding", "sync");
 	image.setAttribute("loading", "eager");
 });
@@ -18,7 +18,6 @@ function blurry() {
 	image.style.opacity = scale(load, 0, 100, 1, 1);
 	image.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
 }
-
 function scale(current, in_min, in_max, out_min, out_max) {
 	return (
 		((current - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
