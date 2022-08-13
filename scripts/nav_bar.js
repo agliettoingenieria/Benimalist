c5390655d9cd64d89367097d2765ed428c4c18a0
IgnFed <ignaciofedorenco2317@gmail.com>
2022-08-13 12:10:32 -0300

    cambiado formato de ruta de los iconos

--- scripts/nav_bar.js
+++ scripts/nav_bar.js
@@ -34,11 +34,11 @@ themeBtn.addEventListener("click", (e) => {
 	if (bodyElement.getAttribute("class") === "bg-light") {
 		bodyElement.classList.replace("bg-light", "bg-dark");
 		bodyTheme = "bg-dark";
-		themeBtnImg.setAttribute("src", "../assets/icons/Moon_Icon.svg");
+		themeBtnImg.setAttribute("src", "/assets/icons/Moon_Icon.svg");
 	} else if (bodyElement.getAttribute("class") === "bg-dark") {
 		bodyElement.classList.replace("bg-dark", "bg-light");
 		bodyTheme = "bg-light";
-		themeBtnImg.setAttribute("src", "../assets/icons/Sun_Icon.svg");
+		themeBtnImg.setAttribute("src", "/assets/icons/Sun_Icon.svg");
 	}
 	window.localStorage.setItem("settings", JSON.stringify({ theme: bodyTheme }));
 });
