981178edd303394218657b869e54d752d5253e0c
IgnFed <ignaciofedorenco2317@gmail.com>
2022-08-13 09:03:40 -0300

    Mejorado estilo mediante codigo

--- scripts/nav_bar.js
+++ scripts/nav_bar.js
@@ -1,11 +1,22 @@
 import { $ } from "./utils.js";
 const burgerBtn = $("#menu_btn");
 const linksList = $(".links_list__list");
+const liElements = linksList.querySelectorAll(".list__item");
+const aElements = linksList.querySelectorAll(".list__item a");
 const themeBtn = $("#theme_btn");
 const nav = $(".nav-bar");
 
 document.addEventListener("DOMContentLoaded", (_) => {
 	nav.style.opacity = 1;
+	const currentPath = window.location.pathname;
+	console.log(currentPath);
+	console.log();
+	aElements.forEach((el, idx) => {
+		if (el.getAttribute("href") === currentPath) {
+			liElements[idx].style.backgroundColor = "#1c3483db";
+			liElements[idx].style.borderRadius = ".375rem";
+		}
+	});
 });
 
 burgerBtn.addEventListener("click", (e) => {
