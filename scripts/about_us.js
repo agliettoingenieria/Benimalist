3425154ed8e7e2a3deb3bdb2dedfe98141c2aa84
IgnFed <ignaciofedorenco2317@gmail.com>
2022-08-14 05:14:23 -0300

    mejorado los textos y la estructura

--- scripts/about_us.js
+++ scripts/about_us.js
@@ -1,18 +1,22 @@
-import { $ } from "./utils.js";
+import { $, $$ } from "./utils.js";
 
 document.addEventListener("DOMContentLoaded", () => {
 	const video = $(".video");
-	const decorator = $("[data-desc-decorator]");
-	const description = $(".about_us__description > p");
+	const decorators = $$("[data-desc-decorator]");
+	const descriptions = $$(".about_us__description > p");
 
 	video.innerHTML = `
 	<iframe src="https://www.youtube.com/embed/8mHNYJVrp8Q" title="YouTube video player" frameborder="0"
 						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
 						allowfullscreen></iframe>
 	`;
-	decorator.style.opacity = 1;
+	for (const decorator of decorators) {
+		decorator.style.opacity = 1;
+	}
 	setTimeout(() => {
-		description.style.opacity = 1;
-		description.style.left = "12px";
+		for (const description of descriptions) {
+			description.style.opacity = 1;
+			description.style.left = "12px";
+		}
 	}, 100);
 });
