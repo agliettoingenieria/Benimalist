ca85e2bfc9db222ba9355cd77dc699bbf3b6b7ab
IgnFed <ignaciofedorenco2317@gmail.com>
2022-08-15 05:22:25 -0300

    Fotos añadidas

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,5 +1,6 @@
 import { $, $$ } from "./utils.js";
-
+import gallery_json from "./images.json" assert { type: "json" };
+let lastImageObserved = 0;
 const gallery = $(".gallery-container");
 
 const observerOptions = {
@@ -22,7 +23,8 @@ const lastImageObserver = new IntersectionObserver(
 	([entry]) => {
 		if (!entry.isIntersecting) return;
 		lastImageObserver.unobserve(entry.target);
-		loadNewCards();
+		if (gallery_json.images.data.length !== lastImageObserved) loadNewCards();
+		console.log("oberving again");
 		lastImageObserver.observe($(".img-container:last-child"));
 	},
 	{
@@ -31,24 +33,31 @@ const lastImageObserver = new IntersectionObserver(
 );
 
 const cards = $$(".img-container");
-const lastCard = $(".img-container:last-child");
 
-lastImageObserver.observe(lastCard);
-
-for (const card of cards) {
-	intersectionObserver.observe(card);
-}
+const normalizeImageURL = ({ dir, name, ext }) => dir + name + "." + ext;
 
 function loadNewCards() {
+	const { images, ext, abs_dir } = gallery_json;
 	const nodes = [];
-	for (let i = 0; i < 10; i++) {
+	for (let i = lastImageObserved; i < images.data.length - 1; i++) {
 		const newCard = document.createElement("article");
 		newCard.setAttribute("class", "img-container");
 		const newImage = new Image();
-		newImage.src = "/assets/images/EXTERIOR_48 - Foto 2.jpg";
+		newImage.src = images.data[i].dir;
 		newCard.appendChild(newImage);
 		intersectionObserver.observe(newCard);
 		nodes.push(newCard);
+		lastImageObserved++;
 	}
 	gallery.append(...nodes);
+}
+
+if (cards.length === 0) {
+	loadNewCards();
+	const lastCard = $(".img-container:last-child");
+	console.log(lastCard);
+	lastImageObserver.observe(lastCard);
+	for (const card of cards) {
+		intersectionObserver.observe(card);
+	}
 }
