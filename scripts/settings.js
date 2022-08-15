05ed65a81c2fa8cbe71e28beafba126797ac2425
IgnFed <ignaciofedorenco2317@gmail.com>
2022-08-14 21:03:52 -0300

    Arreglado problema de flash blanco al cargar html

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,4 +1,18 @@
+import { $ } from "./utils.js";
 const localStorage = JSON.parse(window.localStorage.getItem("settings")) || {
 	theme: "bg-dark",
 };
-document.body.classList.add(localStorage.theme);
+function showTheme() {
+	$("html").style.backgroundColor =
+		localStorage.theme === "bg-dark" && "#1f1d1d";
+	document.body.classList.add(localStorage.theme);
+}
+function showContent() {
+	document.body.style.visibility = "visible";
+	document.body.style.opacity = "1";
+}
+
+showTheme();
+window.addEventListener("DOMContentLoaded", (_) => {
+	showContent();
+});
