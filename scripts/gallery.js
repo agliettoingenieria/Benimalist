fbbfbe67151632d41cc07570453506d33416a88f
IgnFed <ignaciofedorenco2317@gmail.com>
2022-08-16 03:47:55 -0300

    Tratando de obtener una mejor performance

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,71 +1,64 @@
-import { $, $$, Observer, timeout } from "./utils.js";
+import { $, Observer, timeout } from "./utils.js";
 import gallery_json from "./images.json" assert { type: "json" };
 let lastImageObserved = 0;
-let imagesPerRequest = 5;
-
-let galleryObserver, lastCardObserver;
-
+let imagesPerRequest = 4;
+let lastCardObserver;
 const gallery = $(".gallery-container");
 
-function galleryObserverCallback(entries) {
-	for (const [idx, entry] of entries.entries()) {
-		entry.target.addEventListener("transitionend", ({ target }) => {
-			target.style.transitionDelay = "0s";
-		});
-		entry.target.style.transitionDelay = `${(idx + 1) / 10}s`;
-		entry.target.classList.toggle("show", entry.isIntersecting);
+window.addEventListener("load", main);
+
+function main() {
+	function lastCardObserverCallback([entry]) {
+		if (!entry.isIntersecting) return;
+		lastCardObserver.unobserve(entry.target);
+		timeout(() => {
+			if (gallery_json.images.data.length !== lastImageObserved) loadNewCards();
+			if (lastImageObserved !== gallery_json.images.data.length - 1) {
+				lastCardObserver.observe($(".img-container:last-child"));
+			}
+		}, 800);
 	}
-}
 
-function lastCardObserverCallback([entry]) {
-	if (!entry.isIntersecting) return;
-	lastCardObserver.unobserve(entry.target);
-	timeout(() => {
-		if (gallery_json.images.data.length !== lastImageObserved) loadNewCards();
-		if (lastImageObserved !== gallery_json.images.data.length - 1) {
-			lastCardObserver.observe($(".img-container:last-child"));
+	function loadNewCards() {
+		const { images } = gallery_json;
+		const nodes = [];
+		for (let i = 0; i < imagesPerRequest; i++) {
+			if (lastImageObserved >= images.data.length - 1) break;
+			const newCard = document.createElement("article");
+			newCard.setAttribute("class", "img-container");
+			const dir = images.data[lastImageObserved].dir;
+
+			const newImage = setImageProperies(dir, () => {
+				newCard.classList.add("show");
+			});
+
+			newCard.appendChild(newImage);
+			nodes.push(newCard);
+			lastImageObserved++;
 		}
-	}, 400);
-}
+		gallery.append(...nodes);
+	}
 
-function loadNewCards() {
-	const { images } = gallery_json;
-	const nodes = [];
-	for (let i = 0; i < imagesPerRequest; i++) {
-		if (lastImageObserved >= gallery_json.images.data.length - 1) break;
-		const newCard = document.createElement("article");
-		newCard.setAttribute("class", "img-container");
+	function setImageProperies(imageSrc, loadCb) {
 		const newImage = new Image();
 		newImage.loading = "lazy";
 		newImage.decoding = "async";
-		newImage.addEventListener("load", (e) => {
-			newCard.style.opacity = 1;
-			newCard.style.left = 0;
-		});
-		newImage.src = images.data[lastImageObserved].dir;
-		newCard.appendChild(newImage);
-		galleryObserver.observe(newCard);
-		nodes.push(newCard);
-		lastImageObserved++;
+		newImage.src = imageSrc;
+		newImage.addEventListener("load", loadCb);
+		return newImage;
 	}
-	gallery.append(...nodes);
-}
 
-const cards = $$(".img-container");
-const observerOptions = {
-	root: null,
-	rootMargin: "-20px",
-};
+	const observerOptions = {
+		rootMargin: "0px 0px -80px 0px",
+		threshold: 0.5,
+	};
 
-galleryObserver = Observer(null, galleryObserverCallback, observerOptions);
-loadNewCards();
-const lastCard = $(".img-container:last-child");
-lastCardObserver = Observer(
-	lastCard,
-	lastCardObserverCallback,
-	observerOptions
-);
-lastCardObserver.observe();
-for (const card of cards) {
-	galleryObserver.observe(card);
+	loadNewCards();
+	const lastCard = $(".img-container:last-child");
+	lastCardObserver = Observer(
+		lastCard,
+		lastCardObserverCallback,
+		observerOptions
+	);
+	lastCardObserver.observe();
 }
