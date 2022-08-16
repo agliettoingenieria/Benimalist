e1818214744873daa3549e895b8c98b913d4e8ea
IgnFed <ignaciofedorenco2317@gmail.com>
2022-08-15 23:09:58 -0300

    La carga del video es ahora diferida

--- scripts/about_us.js
+++ scripts/about_us.js
@@ -2,21 +2,42 @@ import { $, $$ } from "./utils.js";
 
 document.addEventListener("DOMContentLoaded", () => {
 	const video = $(".video");
+	const socials = $$(".socials__container_list .icon");
 	const decorators = $$("[data-desc-decorator]");
 	const descriptions = $$(".about_us__description > p");
 
-	video.innerHTML = `
-	<iframe src="https://www.youtube.com/embed/8mHNYJVrp8Q" title="YouTube video player" frameborder="0"
-						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-						allowfullscreen></iframe>
-	`;
 	for (const decorator of decorators) {
 		decorator.style.opacity = 1;
 	}
+	console.log(socials);
 	setTimeout(() => {
 		for (const description of descriptions) {
 			description.style.opacity = 1;
 			description.style.left = "12px";
 		}
-	}, 100);
+		console.log(socials);
+		for (const [idx, social] of socials.entries()) {
+			social.style.animationName = "appear";
+			social.style.animationDuration = ".8s";
+			social.style.animationDelay = `${(idx + 0.2) / 10}s`;
+		}
+	}, 300);
+
+	const intersectionObserver = new IntersectionObserver(
+		([target]) => {
+			if (target.isIntersecting) {
+				video.innerHTML = `
+		<iframe src="https://www.youtube.com/embed/8mHNYJVrp8Q" title="YouTube video player" frameborder="0"
+							allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+							allowfullscreen></iframe>
+		`;
+			}
+		},
+		{
+			root: null,
+			rootMargin: "80px",
+		}
+	);
+
+	intersectionObserver.observe(video);
 });
