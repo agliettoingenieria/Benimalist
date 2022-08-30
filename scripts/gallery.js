666e2ab4cdda519be021ba533e658804cf4c16b5
IgnFed <ignaciofedorenco2317@gmail.com>
2022-08-30 15:14:52 -0300

    Implementado un modal para mostrar una imagen de mayor tamaño

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -3,10 +3,19 @@ import gallery_json from "./images.js";
 
 window.addEventListener("load", (e) => {
 	const gallery = $(".gallery-container");
+	const growImage = $(".grow-image-container");
+	const closeImageBtn = $("#close-image");
+	const imgSized = $(".img-sized");
+
 	let lastImageObserved = 0;
 	let imagesPerRequest = 2;
 
 	let galleryObserver, lastCardObserver;
+
+	closeImageBtn.addEventListener("click", () => {
+		growImage.classList.toggle("show");
+	});
+
 	function galleryObserverCallback(entries) {
 		for (const [idx, entry] of entries.entries()) {
 			entry.target.classList.toggle("show", entry.isIntersecting);
@@ -28,6 +37,11 @@ window.addEventListener("load", (e) => {
 			const newCard = document.createElement("article");
 			newCard.setAttribute("class", "img-container");
 			const newImage = new Image();
+			newImage.addEventListener("click", (ev) => {
+				const src = ev.target.getAttribute("src");
+				imgSized.setAttribute("src", src);
+				growImage.classList.toggle("show");
+			});
 			newImage.loading = "lazy";
 			newImage.decoding = "async";
 			newImage.src = images.data[lastImageObserved].dir;
